dc66184b6bd101bef7e8ed8191d13a490d702abd
Marcin Gołębiewski <m.golebiewski@outlook.com>
2017-05-28 18:10:10 +0200

    Add files via upload

---
+++ Figury/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Figury
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Prostokat A1 = new Prostokat(5, 3);
+            A1.Pole();
+            A1.Obwód();
+            Prostokat A2 = new Prostokat(14.52, 2);
+            A2.Pole();
+            A2.Obwód();
+            Console.ReadLine();
+        }
+    }
+}
